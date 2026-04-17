66b541ab874110e664aef1dd8f997a377af9b7d3
Igor Maznitsa <rrg4400@gmail.com>
2026-04-17 10:06:25 +0300

    changed folder structure

---
+++ langtrainer-win-launcher/src/go.mod
@@ -0,0 +1,2 @@
+module github.com/raydac/dcf777-soundwave
+        go 1.24.1
--- src/main/java/com/igormaznitsa/langtrainer/modules/dialog/DialogLine.java
+++
@@ -1,4 +0,0 @@
-package com.igormaznitsa.langtrainer.modules.dialog;
-
-public record DialogLine(String a, String b) {
-}
